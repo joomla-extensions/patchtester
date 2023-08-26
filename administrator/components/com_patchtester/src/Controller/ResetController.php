7d069d99150d15b30357ff6ebf18cc985607ba78
Roland Dalmulder <contact@rolandd.com>
2023-08-26 11:34:17 +0200

    Update administrator/components/com_patchtester/src/Controller/ResetController.php
    Co-authored-by: Dimitris Grammatikogiannis <dg@dgrammatiko.dev>

--- administrator/components/com_patchtester/src/Controller/ResetController.php
+++ administrator/components/com_patchtester/src/Controller/ResetController.php
@@ -18,7 +18,7 @@ use Joomla\CMS\Router\Route;
 use Joomla\Component\Patchtester\Administrator\Model\PullModel;
 use Joomla\Component\Patchtester\Administrator\Model\PullsModel;
 use Joomla\Component\Patchtester\Administrator\Model\TestsModel;
-use Joomla\CMS\Filesystem;
+use Joomla\CMS\Filesystem\File;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
